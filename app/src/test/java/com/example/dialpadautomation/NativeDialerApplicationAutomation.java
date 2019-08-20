295e392496187b4c9f18ca53cfc1d7ca757a5c07
BaPpY <asifuzzaman.bappy@gmail.com>
2019-08-20 22:18:27 +0600

    Workout For Native Android Dialpad Automation In Progress.

--- app/src/test/java/com/example/dialpadautomation/NativeDialerApplicationAutomation.java
+++ app/src/test/java/com/example/dialpadautomation/NativeDialerApplicationAutomation.java
@@ -1,8 +1,100 @@
 package com.example.dialpadautomation;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class NativeDialerApplicationAutomation {
 
+    // This will show you Focused App in Your Emulator :
+    // adb shell "dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp'"
 
+    AppiumDriver<MobileElement> appium_Driver;
+
+    @BeforeTest
+    public void settingEnvironment() throws MalformedURLException {
+        // Created object of DesiredCapabilities class.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        // Set android deviceName desired capability. Set your device name.
+        capabilities.setCapability("deviceName", "emulator-5554");
+
+        // Set BROWSER_NAME desired capability. It's Android in our case here.
+        //capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+
+        // Set android VERSION desired capability. Set your mobile device's OS version.
+        capabilities.setCapability(CapabilityType.VERSION, "7.1.1");
+
+        // Set android platformName desired capability. It's Android in our case here.
+        capabilities.setCapability("platformName", "Android");
+
+        // Set android appPackage desired capability. It is
+        // com.android.calculator2 for calculator application.
+        // Set your application's appPackage if you are using any other app.
+            capabilities.setCapability("appPackage", "com.android.dialer");
+
+        // Set android appActivity desired capability. It is
+        // com.android.calculator2.Calculator for calculator application.
+        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability("appActivity", "com.android.dialer.DialtactsActivity");
+
+        // Created object of RemoteWebDriver will all set capabilities.
+        // Set appium server address and port number in URL string.
+        // It will launch calculator app in android device.
+        appium_Driver = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        appium_Driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void CommencingTest(){
+
+        appium_Driver.findElement(By.id("9")).click();
+
+//        appium_Driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout" +
+//                "/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.FrameLayout")).click();
+
+        appium_Driver.findElement(By.name("+")).click();
+
+        appium_Driver.findElement(By.name("8")).click();
+
+        appium_Driver.findElement(By.name("8")).click();
+
+        appium_Driver.findElement(By.name("0")).click();
+
+        appium_Driver.findElement(By.name("1")).click();
+
+        appium_Driver.findElement(By.name("9")).click();
+
+        appium_Driver.findElement(By.name("1")).click();
+
+        appium_Driver.findElement(By.name("5")).click();
+
+        appium_Driver.findElement(By.name("6")).click();
+
+        appium_Driver.findElement(By.name("4")).click();
+
+        //appium_Driver.findElement(By.id("com.android.dialer:id/dialpad_key_number")).click();
+
+        appium_Driver.findElement(By.id("com.android.dialer:id/dialpad")).click();
+
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+        appium_Driver.quit();
+        System.out.print("Test Completed");
+    }
 
 
 }
